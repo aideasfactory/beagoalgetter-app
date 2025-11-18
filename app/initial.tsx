298b123377e59a802170b9878b138dc65a904625
sam@aideasfactory.io <sam@aideasfactory.io>
2025-11-18 22:31:45 +0000

    redirect

--- app/initial.tsx
+++ app/initial.tsx
@@ -1,31 +1,7 @@
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
-import { useTranslation } from 'react-i18next';
-import { AppLogo, Button } from '@/components';
-import { useSession } from '@/context';
+import { Redirect } from 'expo-router';
 
 const InitialScreen = () => {
-  const { signInWithSkip } = useSession()
-
-  const { t } = useTranslation();
-
-  return (
-    <View className='flex-1 w-full h-full justify-center items-center pr-6 pl-6'>
-      <View className='mb-8'>
-        <AppLogo />
-      </View>
-      <Text className='mb-2 text-2xl font-bold'>{t('welcome')}</Text>
-      <Text className='mb-16'>{t('startBuildingYourApp')}</Text>
-      <View className='w-full'> 
-        <Link href={'/signup'} asChild>
-          <Button title={t('createAnAccount')} className='mb-0 mt-0' />
-        </Link>
-        <Link href={'/login'} asChild>
-          <Button title={t('alreadyHaveAccount')} className='mt-0 mb-0' />
-        </Link>
-      </View>
-    </View>
-  );
+  return <Redirect href="/login" />;
 }
 
 export default InitialScreen
